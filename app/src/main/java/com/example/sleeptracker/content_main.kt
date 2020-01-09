e5cdcf3ab152ed1f66a3126134662dd236a2b05b
fongjs-wm17@student.tarc.edu.my <fjs991204>
2020-01-09 11:04:26 +0800

    initial

---
+++ app/src/main/java/com/example/sleeptracker/content_main.kt
@@ -0,0 +1,12 @@
+package com.example.sleeptracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class content_main : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_content_main)
+    }
+}
---
+++ app/src/main/java/com/example/sleeptracker/recyclerView_item.kt
@@ -0,0 +1,12 @@
+package com.example.sleeptracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class recyclerView_item : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view_item)
+    }
+}
